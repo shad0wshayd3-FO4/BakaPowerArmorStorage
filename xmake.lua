664c90042cbb3d18a2e700291c36d7fc3c6b2594
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-30 04:41:44 -0600

    revert: release impl

--- xmake.lua
+++ xmake.lua
@@ -31,7 +31,3 @@ target("BakaPowerArmorStorage")
 
     -- add extra files
     add_extrafiles(".clang-format")
-
-    -- add install files
-    add_installfiles("res/Interface/*.swf", { prefixdir = "Interface" })
-    add_installfiles("res/MCM/**.*",        { prefixdir = "MCM/config/BakaPowerArmorStorage" })
